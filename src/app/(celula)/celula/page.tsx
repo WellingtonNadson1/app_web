d1d642b114e7715a795aa316e287f43bf027ea14
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-08-21 15:43:40 -0300

    conditional get data celula console.log

--- src/app/(celula)/celula/page.tsx
+++ src/app/(celula)/celula/page.tsx
@@ -2,6 +2,7 @@
 import Calendar from '@/components/Calendar'
 // import Header from '@/components/Header'
 import LicoesCelula from '@/components/LicoesCelula'
+import SpinnerButton from '@/components/spinners/SpinnerButton'
 import { fetchWithToken } from '@/functions/functions'
 import { useSession } from 'next-auth/react'
 import useSWR from 'swr'
@@ -11,7 +12,9 @@ export default function ControleCelulaSupervision() {
   const { data: session } = useSession()
   const hostname = 'app-ibb.onrender.com'
   console.log(`Use session CelulaID: ${session?.user.celulaId}`)
-  const URL = `https://${hostname}/celulas/${session?.user.celulaId}`
+  const URL = session?.user.celulaId
+    ? `https://${hostname}/celulas/${session.user.celulaId}`
+    : null
 
   const {
     data: celula,
@@ -21,6 +24,10 @@ export default function ControleCelulaSupervision() {
     [URL, `${session?.user.token}`],
     ([url, token]: [string, string]) => fetchWithToken(url, token),
   )
+
+  if (!session) {
+    return <SpinnerButton />
+  }
 
   if (error) {
     return (
@@ -47,29 +54,7 @@ export default function ControleCelulaSupervision() {
         {!isLoading ? (
           celula && <ControlePresencaCelula celula={celula} />
         ) : (
-          <div className="flex items-center justify-center gap-2">
-            <svg
-              className="mr-3 h-5 w-5 animate-spin text-gray-400"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-            carregando...
-          </div>
+          <SpinnerButton />
         )}
       </div>
     </div>
--- src/components/spinners/SpinnerButton.tsx
+++ src/components/spinners/SpinnerButton.tsx
@@ -1,13 +1,27 @@
 export default function SpinnerButton() {
   return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <div className="spinner">
-          <div className="flex h-screen items-center justify-center">
-            <div className="spinner h-16 w-16 animate-spin rounded-full border-4 border-t-4 border-gray-200"></div>
-          </div>
-        </div>
-      </div>
-    </>
+    <div className="flex items-center justify-center gap-2">
+      <svg
+        className="mr-3 h-5 w-5 animate-spin text-gray-400"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+      carregando...
+    </div>
   )
 }
