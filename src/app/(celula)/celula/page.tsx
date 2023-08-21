cf9304231440b34812a721b9ba187cd12821b026
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-08-21 03:16:58 -0300

    conditional get data celula

--- src/app/(celula)/celula/page.tsx
+++ src/app/(celula)/celula/page.tsx
@@ -33,16 +33,6 @@ export default function ControleCelulaSupervision() {
     )
   }
 
-  if (isLoading) {
-    return (
-      <div className="mx-auto w-full px-2 py-2">
-        <div className="mx-auto flex w-full items-center gap-2">
-          <div className="text-white">carregando...</div>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className="relative mx-auto w-full px-2 py-2">
       <div className="relative mx-auto w-full">
@@ -55,7 +45,33 @@ export default function ControleCelulaSupervision() {
         <LicoesCelula />
       </div>
       <div className="relative mx-auto mb-4 w-full px-2">
-        {celula && <ControlePresencaCelula celula={celula} />}
+        {!isLoading ? (
+          celula && <ControlePresencaCelula celula={celula} />
+        ) : (
+          <div className="flex items-center justify-center gap-2">
+            <svg
+              className="mr-3 h-5 w-5 animate-spin text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            carregando...
+          </div>
+        )}
       </div>
     </div>
   )
