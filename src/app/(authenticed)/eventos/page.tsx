648e374c4752ae163fdf60bcb03f2f1b9f2b2e24
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-08-22 17:43:23 -0300

    Cheange de struture of modal button

--- src/app/(authenticed)/eventos/page.tsx
+++ src/app/(authenticed)/eventos/page.tsx
@@ -49,17 +49,17 @@ export default function Cultos() {
         setIsLoadingSubmitForm(false)
         setFormSuccess(true)
         success('Culto Cadastrado')
-        setShowModal(!showModal)
+        setShowModal(false)
       } else {
         errorCadastro('Erro ao cadastrar Culto')
         setIsLoadingSubmitForm(false)
-        setShowModal(!showModal)
+        setShowModal(false)
       }
     } catch (error) {
       console.log(error)
       errorCadastro('Erro ao cadastrar Culto')
       setIsLoadingSubmitForm(false)
-      setShowModal(!showModal)
+      setShowModal(false)
     }
     reset()
   }
@@ -201,33 +201,35 @@ export default function Cultos() {
                     Cancelar
                   </button>
                   {isLoadingSubmitForm ? (
-                    <button
-                      type="submit"
-                      disabled={isLoadingSubmitForm}
-                      className="rounded-md bg-green-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700"
-                    >
-                      <svg
-                        className="mr-3 h-5 w-5 animate-spin text-gray-400"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                      <button
+                        type="submit"
+                        disabled={isLoadingSubmitForm}
+                        className="rounded-md bg-green-700 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-700"
                       >
-                        <circle
-                          className="opacity-25"
-                          cx="12"
-                          cy="12"
-                          r="10"
-                          stroke="currentColor"
-                          strokeWidth="4"
-                        ></circle>
-                        <path
-                          className="opacity-75"
-                          fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                      </svg>
-                      <span>Cadastrando...</span>
-                    </button>
+                        <svg
+                          className="mr-3 h-5 w-5 animate-spin text-gray-400"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          ></path>
+                        </svg>
+                        <span>Cadastrando...</span>
+                      </button>
+                    </div>
                   ) : (
                     <button
                       type="submit"
