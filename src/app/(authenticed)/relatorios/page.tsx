6d8a6c940ac79fe1ba626de76323f16b31bb6994
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-09-04 00:35:35 -0300

    Add section relatorios

--- src/app/(authenticed)/relatorios/page.tsx
+++ src/app/(authenticed)/relatorios/page.tsx
@@ -1,9 +1,10 @@
-'use client'
+import StatsCardRelatorios from '@/components/StatsCardRelatoros'
 
 export default function Relatorios() {
   return (
     <div className="mx-auto w-full px-2 py-2">
-      <h2>Relatórios</h2>
+      <div className="mx-auto w-full"></div>
+      <StatsCardRelatorios />
     </div>
   )
 }
