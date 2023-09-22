0f493a29d737afbc9af114314f32d0a62b848cff
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-09-22 11:18:06 -0300

    Change in the attribute newRefresktoken for refreshtoken

--- src/lib/hooks/useRefreshToken.ts
+++ src/lib/hooks/useRefreshToken.ts
@@ -20,24 +20,27 @@ export const useRefreshToken = () => {
         const newToken = response.data
         console.log('New Token Function Refresh', newToken)
 
-        if (newToken) session.user.token = response.data.token
+        if (newToken) {
+          session.user.token = response.data.token
+          session.user.refreshToken = response.data.newRefreshToken
+        }
       }
     }
 
-    if (session?.user.token && session?.user.newRefreshToken) {
-      // Verificar se o token de acesso expirou
-      const isTokenExpired = dayjs().isAfter(dayjs.unix(session?.user.newRefreshToken.expiresIn as number));
+    // if (session?.user.token && session?.user.newRefreshToken) {
+    //   // Verificar se o token de acesso expirou
+    //   const isTokenExpired = dayjs().isAfter(dayjs.unix(session?.user.newRefreshToken.expiresIn as number));
 
-      if (isTokenExpired) {
-        const response = await axios.post('/refresh-token', {
-          refresh_token: session?.user.newRefreshToken.id
-        })
-        const newToken = response.data
-        console.log('New Token Function NewRefresh', newToken)
+    //   if (isTokenExpired) {
+    //     const response = await axios.post('/refresh-token', {
+    //       refresh_token: session?.user.newRefreshToken.id
+    //     })
+    //     const newToken = response.data
+    //     console.log('New Token Function NewRefresh', newToken)
 
-        if (newToken) session.user.token = response.data.token
-      }
-    }
+    //     if (newToken) session.user.token = response.data.token
+    //   }
+    // }
 
 
   }
