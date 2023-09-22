166f1408c4d17de568ddeb233b6dba8f155571d0
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-09-22 10:26:34 -0300

    add headers in the requests

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,14 +6,16 @@ const BASE_URL = `https://${hostname}`
 export default axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Content-Type": "application/json"
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
   }
 })
 
 export const axiosAuth = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Content-Type": "application/json"
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
   }
 })
 
