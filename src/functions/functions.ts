5279ce045e420af835f78453a8a6421a33e54119
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-09-26 00:20:45 -0300

    point zero

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -11,52 +11,52 @@ export interface FetchError extends AxiosError {}
 const hostname = 'app-ibb.onrender.com'
 export const BASE_URL = `https://${hostname}`
 
-export async function fetchWithToken(
-  url: string,
-  methodType: 'GET' | 'POST' | 'PUT' | 'DELETE',
-  token: string,
-) {
-  const refreshToken = useRefreshToken()
+// export async function fetchWithToken(
+//   url: string,
+//   methodType: 'GET' | 'POST' | 'PUT' | 'DELETE',
+//   token: string,
+// ) {
+//   const refreshToken = useRefreshToken()
 
-  try {
-    console.log('fetchWithToken called with URL:', url)
-    const response = await axios({
-      method: methodType,
-      url: url,
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+//   try {
+//     console.log('fetchWithToken called with URL:', url)
+//     const response = await axios({
+//       method: methodType,
+//       url: url,
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
 
-    console.log('fetchWithToken response data:', response.data)
-    return response.data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const axiosError = error as AxiosError;
-      if (axiosError.response?.status === 401) {
-        // Token expirado, tentar obter um novo token com refreshToken
-        try {
-          const newToken = await refreshToken();
-          // Reenviar a solicitação com o novo token
-          const newResponse = await axios({
-            method: methodType,
-            url: url,
-            headers: {
-              Authorization: `Bearer ${newToken}`,
-            },
-          });
-          return newResponse.data;
-        } catch (refreshError) {
-          console.error('Failed to refresh token:', refreshError);
-          throw new Error('Failed to fetch data with token.');
-        }
-      }
-    }
+//     console.log('fetchWithToken response data:', response.data)
+//     return response.data;
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       const axiosError = error as AxiosError;
+//       if (axiosError.response?.status === 401) {
+//         // Token expirado, tentar obter um novo token com refreshToken
+//         try {
+//           const newToken = await refreshToken();
+//           // Reenviar a solicitação com o novo token
+//           const newResponse = await axios({
+//             method: methodType,
+//             url: url,
+//             headers: {
+//               Authorization: `Bearer ${newToken}`,
+//             },
+//           });
+//           return newResponse.data;
+//         } catch (refreshError) {
+//           console.error('Failed to refresh token:', refreshError);
+//           throw new Error('Failed to fetch data with token.');
+//         }
+//       }
+//     }
 
-    console.error('Unknown error occurred:', error);
-    throw new Error('Unknown error occurred.');
-  }
-}
+//     console.error('Unknown error occurred:', error);
+//     throw new Error('Unknown error occurred.');
+//   }
+// }
 
 // export async function fetchWithToken(
 //   url: string,
@@ -91,34 +91,34 @@ export async function fetchWithToken(
 // const hostname = 'app-ibb.onrender.com'
 // export const BASE_URL = `https://${hostname}`
 
-// export async function fetchWithToken(
-//   url: string,
-//   methodType: 'GET' | 'POST' | 'PUT' | 'DELETE', // Aceita os tipos de método axios permitidos
-//   token: string
-// ) {
-//   try {
-//     const response = await axios({
-//       method: methodType, // Usa o método passado como parâmetro
-//       url: url,
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
+export async function fetchWithToken(
+  url: string,
+  methodType: 'GET' | 'POST' | 'PUT' | 'DELETE', // Aceita os tipos de método axios permitidos
+  token: string
+) {
+  try {
+    const response = await axios({
+      method: methodType, // Usa o método passado como parâmetro
+      url: url,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-//     return response.data;
-//   } catch (error) {
-//     if (axios.isAxiosError(error)) {
-//       const fetchError: FetchError = {
-//         ...error,
-//         message: 'Failed to fetch data with token.',
-//       };
-//       throw fetchError;
-//     } else {
-//       console.error('Unknown error occurred:', error);
-//       throw new Error('Unknown error occurred.');
-//     }
-//   }
-// }
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const fetchError: FetchError = {
+        ...error,
+        message: 'Failed to fetch data with token.',
+      };
+      throw fetchError;
+    } else {
+      console.error('Unknown error occurred:', error);
+      throw new Error('Unknown error occurred.');
+    }
+  }
+}
 
 // Notification success or error Submit Forms
 export const success = (message: string) =>
