06851b795251df3690b8a2748c72c8ec160dad7b
WellingtonNadson1 <wellingtonnadson@gmail.com>
2023-09-30 15:38:12 -0300

    implementation layout report

--- src/app/(authenticed)/relatorios/mokupFrequenciaCulto.tsx
+++ src/app/(authenticed)/relatorios/mokupFrequenciaCulto.tsx
@@ -5,10 +5,39 @@ import useAxiosAuthToken from "@/lib/hooks/useAxiosAuthToken"
 import { useQuery } from "@tanstack/react-query"
 import { useSession } from "next-auth/react"
 
+interface Membro {
+  id: string;
+  first_name: string;
+  celula: {
+      nome: string;
+  };
+}
+
+interface PresencaCulto {
+  id: string;
+  data_inicio_culto: string;
+  data_termino_culto: string;
+  status: string;
+  cultoSemanalId: string;
+  date_create: string;
+  date_update: string;
+}
+
+interface Dados {
+  id: string;
+  status: boolean;
+  userId: string;
+  cultoIndividualId: string;
+  membro: Membro;
+  presenca_culto: PresencaCulto;
+  date_create: string;
+  date_update: string;
+}
+
 const MokupFrequenciaCulto = () => {
   const { data: session } = useSession()
   const axiosAuth = useAxiosAuthToken(session?.user.token as string)
-  const { data: presencas, isLoading, isError } = useQuery<[]>({
+  const { data: presencas, isLoading, isError } = useQuery<Dados[]>({
     queryKey: ['presencaCultos'],
     queryFn: () =>
     axiosAuth.get(`${BASE_URL}/presencacultos`),
@@ -19,8 +48,13 @@ const MokupFrequenciaCulto = () => {
         <p><SpinnerButton /></p>
       ) : (
         <>
-          {presencas?.map((presenca, index) => (
-            <div key={index}>{presenca}</div>
+          {presencas?.map((presenca) => (
+            <div key={presenca.id} className="px-2 py-2 mt-3">
+              <h2>{presenca.membro.celula.nome}</h2>
+              <p>{presenca.membro.first_name}</p>
+              <p>{presenca.presenca_culto.data_inicio_culto}</p>
+              <p>{presenca.status}</p>
+            </div>
 
           ))}
         </>
