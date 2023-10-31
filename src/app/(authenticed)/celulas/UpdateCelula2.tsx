109b5cc78d42bb8a725c9b5665c91f96e9151418
Wellington Nadson <wellingtonnadson@MacBook-Air-de-Wellington.local>
2023-10-31 18:55:35 -0300

    disabled input nome in the update celulas

--- src/app/(authenticed)/celulas/UpdateCelula2.tsx
+++ src/app/(authenticed)/celulas/UpdateCelula2.tsx
@@ -256,8 +256,8 @@ export default function UpdateCelula2({
                                   {...register('nome')}
                                   type="text"
                                   id="nome"
-                                  autoComplete="given-name"
-                                  className="block w-full rounded-md border-0 py-1.5 text-slate-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                                  disabled
+                                  className="disabled:opacity-75 block w-full rounded-md border-0 py-1.5 text-slate-700 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
                                 />
                               </div>
                             </div>
