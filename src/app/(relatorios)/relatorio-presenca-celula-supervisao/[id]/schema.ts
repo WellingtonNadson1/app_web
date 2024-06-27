cf5e6e809757143257ad1f4f110510c9e42ad598
Wellington Nadson <wellingtonnadson@MacBook-Air-de-Wellington.local>
2024-06-27 02:08:41 -0300

    Add almas e visitantes in schema for relatorio celulas

--- src/app/(relatorios)/relatorio-presenca-celula-supervisao/[id]/schema.ts
+++ src/app/(relatorios)/relatorio-presenca-celula-supervisao/[id]/schema.ts
@@ -1,47 +1,55 @@
-import { array, z } from 'zod'
+import { array, z } from "zod";
 
 const Membro = z.object({
   id: z.string(),
   first_name: z.string(),
   last_name: z.string(),
-})
+});
 
-const PresencasMembrisReuniaoCelula = z.array(z.object({
-  status: z.boolean(),
-  membro: Membro
-}))
+const PresencasMembrisReuniaoCelula = z.array(
+  z.object({
+    status: z.boolean(),
+    membro: Membro,
+  }),
+);
 
-const ReunioesCelula = z.array(z.object({
-  id: z.string(),
-  data_reuniao: z.string(),
-  presencas_membros_reuniao_celula: PresencasMembrisReuniaoCelula
-}))
+const ReunioesCelula = z.array(
+  z.object({
+    id: z.string(),
+    data_reuniao: z.string(),
+    visitantes: z.string(),
+    almas_ganhas: z.string(),
+    presencas_membros_reuniao_celula: PresencasMembrisReuniaoCelula,
+  }),
+);
 
 export const CelulaSchema = z.object({
   id: z.string(),
   nome: z.string(),
   lider: z.object({ first_name: z.string() }),
-  membros: z.object({
-    id: z.string(),
-    first_name: z.string(),
-  }).array(),
+  membros: z
+    .object({
+      id: z.string(),
+      first_name: z.string(),
+    })
+    .array(),
   _count: z.object({
-    membros: z.number()
+    membros: z.number(),
   }),
-  reunioes_celula: ReunioesCelula
-})
+  reunioes_celula: ReunioesCelula,
+});
 
 const Supervisor = z.object({
   first_name: z.string(),
-})
+});
 
 export const SupervisionDataSchema = z.object({
   supervisionData: z.object({
     nome: z.string(),
     supervisor: Supervisor,
-    celulas: CelulaSchema.array()
-  })
-})
+    celulas: CelulaSchema.array(),
+  }),
+});
 
 export type TSupervisionData = z.infer<typeof SupervisionDataSchema>;
 export type TCelula = z.infer<typeof CelulaSchema>;
@@ -50,6 +58,6 @@ export const FormRelatorioDataSchema = z.object({
   superVisionId: z.string(),
   startDate: z.string(),
   endDate: z.string(),
-})
+});
 
 export type FormRelatorioSchema = z.infer<typeof FormRelatorioDataSchema>;
