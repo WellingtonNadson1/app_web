cabf7f1a682fa7d8c4fee61a2687bce62f915d9f
Wellington Nadson <wellingtonnadson@MacBook-Air-de-Wellington.local>
2024-06-29 00:19:45 -0300

    Add new component for create culto

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -175,9 +175,9 @@ export default function MyCalendar() {
                           meetings?.some((meeting) =>
                             isSameDay(parseISO(meeting.data_inicio_culto), day),
                           ) && (
-                            <span className="relative flex h-3 w-3">
+                            <span className="relative flex h-2 w-2">
                               <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                              <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                              <span className="relative inline-flex rounded-full h-2 w-2 bg-sky-500"></span>
                             </span>
                           )}
                         {isSunday(day) ? (
