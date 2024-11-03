0b6de75fe78b1392da4cfa215324e38cee7df6f4
Wellington Nadson <wellingtonnadson@MacBook-Air-de-Wellington.local>
2024-11-02 21:42:50 -0300

    adjust in the prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
-import globals from 'globals'
 import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import pluginReact from 'eslint-plugin-react'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -9,4 +10,5 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended
 ]
