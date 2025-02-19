aa38041a5be6ba47113277dcaa98933db47c8150
WellingtonNadson1 <wellingtonnadson@gmail.com>
2025-02-19 11:53:03 -0300

    adicionando layouts nas paginas de registro de discipulados

---
+++ src/app/(private)/(discipuladosregisterliderelula)/discipuladoscelularegister/layout.tsx
@@ -0,0 +1,34 @@
+import '@/./app/globals.css';
+import MySidebar from '@/components/sidebar/MySidebar';
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { Providers } from '@/providers/providers';
+import type { Metadata } from 'next';
+import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'App IBB',
+  description: 'Criado para auxiliar no controle e desenvolvimento da IBB',
+  icons: {
+    icon: ['/favicon.ico'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png'],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt" suppressHydrationWarning>
+      <div className="-z-40 absolute top-0 min-h-[18.75rem] w-full bg-[#1D70B6]/90"></div>
+      <body suppressHydrationWarning className={`antialiased`}>
+        <SidebarProvider>
+          <MySidebar />
+          <Providers>{children}</Providers>
+        </SidebarProvider>
+      </body>
+    </html>
+  );
+}
